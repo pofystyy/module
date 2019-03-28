86b09c8fd6cbf3e6dae7d07ce86c70351e6dc06b
pofystyy <tanya.romas@gmail.com>
2019-03-28 13:52:42 +0200

    add exceptions for file with connect to db

--- lib/lightning_module/connect_to_db.rb
+++ lib/lightning_module/connect_to_db.rb
@@ -1,8 +1,11 @@
+require_relative 'exceptions'
 Dir[File.dirname(__FILE__) + '/../storage/**/*.rb'].each { |file| require file }
 
 module ConnectToDb
   def storage(db = 'redis')
-    # redis, rabbit
+  # redis, rabbit
     Object.const_get("Storages::#{db.capitalize}").instance
+  rescue NameError
+    raise LightningModule::Exceptions::InvalidDatabase, "invalid database name #{db.capitalize}"
   end
 end
